d40cecc880ccc2c1e2d69c1f8b6663bc20782b01
nedalex <alexandru.nedelcu@itsmartsystems.eu>
2020-09-17 19:19:57 +0300

    Designed a little from the news feed (Home fragment) & added the name of the current user in the menu fragment.

--- app/src/main/java/com/teme/fakefacebook/dashboard/fragments/MenuFragment.kt
+++ app/src/main/java/com/teme/fakefacebook/dashboard/fragments/MenuFragment.kt
@@ -1,11 +1,18 @@
 package com.teme.fakefacebook.dashboard.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.toObject
 import com.teme.fakefacebook.R
+import com.teme.fakefacebook.registration.models.User
+import com.teme.fakefacebook.registration.signup.MainActivity
+import kotlinx.android.synthetic.main.fragment_menu.*
 
 class MenuFragment : Fragment() {
 
@@ -15,5 +22,23 @@ class MenuFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_menu, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val uid = FirebaseAuth.getInstance().currentUser?.uid.toString()
+
+        FirebaseFirestore.getInstance().collection("users")
+            .document(uid).get().addOnSuccessListener {
+                val user = it.toObject<User>()
+                name_tv.text = user?.firstName.toString() + " " + user?.lastName.toString()
+            }
+
+        log_out_btn.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            activity?.finish()
+            startActivity(Intent(activity, MainActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/teme/fakefacebook/registration/signup/MainActivity.kt
+++ app/src/main/java/com/teme/fakefacebook/registration/signup/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.teme.fakefacebook.registration.signup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import com.teme.fakefacebook.R
+import com.teme.fakefacebook.dashboard.activities.DashboardActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (FirebaseAuth.getInstance().currentUser != null) {
+            startActivity(Intent(this, DashboardActivity::class.java))
+            finish()
+        }
     }
 
 }
